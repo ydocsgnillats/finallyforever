61290d3ea64a7e76a39a4e3251375d5293d3ae4b
Cody Stallings <ydocsgnillats@gmail.com>
2022-01-04 23:00:09 -0500

    buttons, scroll to top, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const homeRef = useRef<any>(null);
+  const offeringRef = useRef<any>(null);
+  const bookRef = useRef<any>(null);
+  const testimonialRef = useRef<any>(null);
+  const aboutRef = useRef<any>(null);
+
+  const handleHome = () => homeRef?.current?.scrollIntoView({ behavior: "smooth" });
+  const handleOffering = () => offeringRef?.current?.scrollIntoView({ behavior: "smooth" });
+  const handleBook = () => bookRef?.current?.scrollIntoView({ behavior: "smooth" });
+  const handleTestimonial = () => testimonialRef?.current?.scrollIntoView({ behavior: "smooth" });
+  const handleAbout = () => aboutRef?.current?.scrollIntoView({ behavior: "smooth" });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <button className="scrollTop" onClick={handleHome}>Scroll To Top</button>
+      <div ref={homeRef} className="Container">
+        <div className="Navbar">
+          <button onClick={handleOffering}>Offerings</button>
+          <button onClick={handleBook}>Book Us</button>
+          <button onClick={handleTestimonial}>Testimonials</button>
+          <button onClick={handleAbout}>About Us</button>
+        </div>
+        <header>
+          <p>
+            It's Finally Forever...
+          </p>
+        </header>
+      </div>
+      <div ref={offeringRef} className="Container">
         <p>
-          It's Finally Forever...
+          Offerings
         </p>
-      </header>
+      </div>
+      <div ref={bookRef} className="Container">
+        <p>
+          Book Us
+        </p>
+      </div>
+      <div ref={testimonialRef} className="Container">
+        <p>
+          Testimonials
+        </p>
+      </div>
+      <div ref={aboutRef} className="Container">
+        <p>
+          About Us
+        </p>
+      </div>
     </div>
   );
 }
